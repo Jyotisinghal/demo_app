abef921fb1febc16367e48eb70a5a3b2e58a8dc8
jyotisinghal <jsinghal694@gmail.com>
2019-10-14 13:29:07 +0530

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,17 @@ Rails.application.routes.draw do
   get '/login',   to: 'sessions#new'
   post '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users do
+    member do                      #can use collection place of member which not consider id like users/following
+      get :following, :followers
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  # resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+
   # root 'application#hello'
 end
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -3,13 +3,14 @@ require 'test_helper'
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:malory)
   end
 
   test "micropost interface" do
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
-    assert_select 'input[type=image]'
+    assert_select 'input[type=submit]'
     # Invalid submission
     # assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
@@ -17,11 +18,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid submission
     content = "This micropost really ties the room together"
-    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
+    picture = fixture_file_upload('test/fixtures/files/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
       post microposts_path, params: { micropost: { content: content , picture: picture} }
     end
-    assert Micropost.picture?
+    assert @user.microposts
     follow_redirect!
     assert_match content, response.body
     # Delete post
@@ -38,13 +39,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   test "micropost sidebar count" do
   	log_in_as(@user)
   	get root_path
-  	assert_match "#{FILL_IN} microposts", response.body
-  	other_user = users(:malory)
-  	log_in_as(other_user)
+  	assert_match "#{@user.microposts.count} microposts", response.body
+  	log_in_as(@other_user)
   	get root_path
-  	assert_match "0 microposts", response.body
-  	other_user.microposts.create!(content: "A micropost")
+  	assert_match "#{@other_user.microposts.count} microposts", response.body
+  	@other_user.microposts.create!(content: "A micropost")
   	get root_path
-  	assert_match FILL_IN,response.body
+  	assert_match root_path,response.body
   end
 end
