01049bf36bbf19a51b347ecb3976327bb4a6404f
jyotisinghal <jsinghal694@gmail.com>
2019-10-14 13:44:29 +0530

    change production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,18 +89,16 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'young-plateau-29376.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://young-plateau-29376.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.perform_deliveries = true
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :port           =>  587,
     :authentication => :plain,
     :user_name      => 'jsinghal694@gmail.com',
     :password       => '1234@@@@',
-    :domain         => 'gmail.com',
+    :domain         => 'gmail.com'
   }
 end
